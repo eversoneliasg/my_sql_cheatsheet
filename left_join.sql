07962070d290f8638cfaf492febe283b57448b57
Everson Elias Goncalves de Oliveira <eversoneliasg@gmail.com>
2022-07-26 10:05:59 -0300

    Left join

---
+++ left_join.sql
@@ -0,0 +1,17 @@
+/*
+* Working with left joins
+*/
+
+SELECT  a.emp_no,
+        CONCAT(a.first_name, ' ', a.last_name) as "name",
+        b.salary,
+        COALESCE(c.title, 'No title change'),
+        COALESCE(c.from_date::text, '-') AS "Title taken on"
+FROM "public"."employees" as a
+INNER JOIN "public"."salaries" AS b ON a.emp_no = b.emp_no
+LEFT JOIN "public"."titles" AS c
+ON  c.emp_no = a.emp_no AND (
+    c.from_date = (b.from_date + interval '2 days') OR
+    c.from_date = b.from_date
+)
+ORDER BY a.emp_no;
