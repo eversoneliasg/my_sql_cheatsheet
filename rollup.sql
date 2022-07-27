25919e011300cb4be7f5192696db452c9b57dede
Everson Elias Goncalves de Oliveira <eversoneliasg@gmail.com>
2022-07-27 20:36:58 -0300

    Rollup

---
+++ rollup.sql
@@ -0,0 +1,15 @@
+SELECT  EXTRACT (YEAR FROM orderdate) AS "year",
+        EXTRACT (MONTH FROM orderdate) AS "month",
+        EXTRACT (DAY FROM orderdate) AS "day",
+        sum(ol.quantity)
+FROM orderlines AS ol
+GROUP BY
+    ROLLUP (
+        EXTRACT (year FROM orderdate),
+        EXTRACT (MONTH FROM orderdate),
+        EXTRACT (DAY FROM orderdate)
+    )
+ ORDER BY
+    EXTRACT (YEAR FROM orderdate),
+    EXTRACT (MONTH FROM orderdate),
+        EXTRACT (DAY FROM orderdate)
