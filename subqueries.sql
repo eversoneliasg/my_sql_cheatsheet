8692f52e4aa423a99bd024b0016c241e951dc5d0
Everson Elias Goncalves de Oliveira <eversoneliasg@gmail.com>
2022-07-29 15:25:30 -0300

    Subqueries - pt 2

--- subqueries.sql
+++ subqueries.sql
@@ -35,3 +35,60 @@ SELECT
 FROM salaries AS s
 LEFT OUTER JOIN titles AS t USING(emp_no, from_date)
 ORDER BY emp_no;
+
+-- Query for most recent salary
+
+SELECT
+    emp_no,
+    salary AS "most recent salary",
+    from_date
+FROM salaries AS s
+WHERE from_date = (
+    SELECT MAX(from_date)
+    FROM salaries AS sp
+    WHERE sp.emp_no = s.emp_no
+    )
+ORDER BY emp_no ASC;
+
+/* TRY TO WRITE THESE AS JOINS FIRST */
+/*
+* DB: Store
+* Table: orders
+* Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
+* ordered by orderid
+*/
+
+SELECT c.firstname, c.lastname, o.orderid 
+FROM orders AS o, (
+    SELECT customerid, state, firstname, lastname
+    FROM customers
+) AS c
+WHERE  o.customerid = c.customerid AND 
+c.state IN ('NY', 'OH', 'OR')
+ORDER BY o.orderid;
+
+/*
+* DB: Employees
+* Table: employees
+* Question: Filter employees who have emp_no 110183 as a manager
+*/
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_emp
+    WHERE dept_no = (
+        SELECT dept_no 
+        FROM dept_manager
+        WHERE emp_no = 110183
+    )
+)
+ORDER BY emp_no
+
+-- Written with JOIN
+SELECT e.emp_no, first_name, last_name
+FROM employees as e
+JOIN dept_emp as de USING (emp_no)
+JOIN dept_manager as dm USING (dept_no)
+WHERE dm.emp_no = 110183
